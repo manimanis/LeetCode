d7700f7bc3238f86ad1796791008c6343a17587f
Mohamed Anis MANI <manimohamed@gmail.com>
2021-04-27 07:21:55 +0100

    Added april 26 problem

--- tests/test_april.py
+++ tests/test_april.py
@@ -16,13 +16,27 @@ class TestSolutionApril(unittest.TestCase):
                 [[15, 13, 2, 5], [14, 3, 4, 1],
                  [12, 6, 8, 9], [16, 7, 10, 11]]  # out
             ),
-            ([[1]], [[2]])
+            ([[1]], [[1]])
         ]
         sol = Solution()
         for inp, out in inputs:
             sol.rotate(inp)
             self.assertEqual(out, inp)
 
+    def testSol26Apr(self):
+        from April2021.sol_26_slow import Solution as Solution1
+        from April2021.sol_26_fast import Solution as Solution2, test_cases
+        
+        sol = Solution1()
+        for inp, out in test_cases:
+            res = sol.furthestBuilding(*inp)
+            self.assertEqual(out, res)
+
+        sol = Solution2()
+        for inp, out in test_cases:
+            res = sol.furthestBuilding(*inp)
+            self.assertEqual(out, res)
+        
 
 if __name__ == '__main__':
     unittest.main()
