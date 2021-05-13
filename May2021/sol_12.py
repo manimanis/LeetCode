2cdf6406f570fda51be25f8da54d6f539f67912b
Mohamed Anis MANI <manimohamed@gmail.com>
2021-05-13 13:28:06 +0100

    Explication de la solution du 12 Mai 2021

--- May2021/sol_12.py
+++ May2021/sol_12.py
@@ -84,6 +84,59 @@ class NumMatrix:
         return r2c2 - r1c2 - r2c1 + r1c1
 
 
+"""
+Explication :
+Prenons la matice suivante :
+3 0 1 4 2
+5 6 3 2 1
+1 2 0 1 5
+4 1 0 1 7
+1 0 3 0 5
+Figure 1, Matrice donnée
+
+On veut calculer la somme des éléments de la sous-matrice, qui est égale à 8 :
+2 0 1
+1 0 1
+0 3 0
+Figure 2, Matrice à calculer
+
+La méthode la plus simple et la plus intuitive est de faire deux compteurs
+sur les lignes et les colonnes à additionner et le tour est joué.
+
+Seulement, dans ce problème il s'agira de calculer plusieurs sommes de 
+sous-matrices et cette méthode risque de "Time Out".
+
+Il faudra, donc, chercher une autre méthode. J'ai pensé à faire le cumul de
+toutes les cellules, ainsi on calcule pour chacune des cellules de cette matrice
+mp[l, c] = somme(i=0..l, j=0..c, m[i, j])
+
+On se retrouve donc avec l'exemple précédent avec la matrice suivante :
+  3   3  4   8  10
+[ 8] 14 18 [24] 27
+  9  17 21  28  36
+ 13  22 26  34  49
+[14] 23 30 [38] 58
+Figure 3, Somme de matrice précalculée, mp
+
+Ainsi, par exemple, 26 correspond à la somme de la sous-matrice :
+3 0 1
+5 6 3
+1 2 0
+4 1 0
+Figure 4, mp[2, 3] = somme(i=0..2, j=0..3, m[i, j])
+
+On peut remarquer que :
+somme(i=r1..r2, j=c1..c2, m[i,j]) = 
+  somme(i=0..r2, j=0..c2, m[i,j]) - 
+  somme(i=0..r2, j=0..c1-1, m[i,j]) - 
+  somme(i=0..r1-1, j=0..c2, m[i, j]) +
+  somme(i=0..r1-1, j=0..c1-1, m[i, j]) =
+    mp[r2, c2] - mp[r2, c1 - 1] - mp[r1-1, c2] + mp[r1-1, c1-1]
+
+Comme nous avons déjà calculé toutes ces sommes, marqués dans la figure 3 entre 
+[]. => 38 - 24 - 14 + 8 = 8
+"""
+
 if __name__ == '__main__':
     for inp, out in test_cases:
         matrix = NumMatrix(inp[0][0])
