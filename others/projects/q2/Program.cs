28c6bc42f5e8f84f6c493ae5350847a960a6b6bb
Mohamed Anis MANI <manimohamed@gmail.com>
2021-05-15 09:28:27 +0100

    Added May 15th, 2021 Solution

---
+++ others/projects/q2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a string ?");
+            string input = Console.ReadLine();
+
+            // there is at most input.Length / 2 words in the input
+            string[] words = new string[input.Length];
+            int count = 0;
+            int p = -1;
+
+            do
+            {
+                // Search the space index
+                p = input.IndexOfAny(new char[] {' ', ',', '.', ':'});
+                // If there is no spaces we are at the last word
+                if (p == -1)
+                {
+                    p = input.Length;
+                }
+                // Get the word
+                string word = input.Substring(0, p);
+                if (word.Length > 0) {
+                    words[count++] = word;
+                }
+                // Remove it from the input string
+                input = (p == input.Length) ? "" : input.Substring(p + 1);
+
+                // Loop until the string is empty
+            } while (input.Length > 0);
+
+            Console.WriteLine("Found {0} words", count);
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+            // The book:on the table, and the pen.
+        }
+    }
+}
