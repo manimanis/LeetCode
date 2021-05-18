652ab4b6d7ed065228bf3ff757db2c665b334be1
manianis <manianis@yahoo.fr>
2021-05-18 11:03:29 +0100

    Added Sol 17th May

--- May2021/sol_17.py
+++ May2021/sol_17.py
@@ -35,13 +35,62 @@ from common.testcases import make_tests
 
 test_cases = [
     ((["a","b","ba","bca","bda","bdca"],), 4),
-    ((["xbc","pcxbcf","xb","cxbc","pcxbc"],), 5)
+    ((["xbc","pcxbcf","xb","cxbc","pcxbc"],), 5),
+    ((["a","ab","ac","bd","abc","abd","abdd"],), 4)
 ]
 
 
 class Solution:
+    @staticmethod
+    def find(words, word, l = None, r = None):
+        if l is None or r is None:
+            l, r = 0, len(words)-1
+        if l <= r:
+            m = (l + r) // 2
+            if words[m] == word:
+                return m
+            ridx = -1
+            if len(words[m]) >= len(word):
+                ridx = Solution.find(words, word, m+1, r)
+            if ridx == -1 and len(words[m]) <= len(word):
+                return Solution.find(words, word, l, m-1)
+            return ridx
+        return -1
+
+    @staticmethod
+    def longest(word, words, map=None):
+        #print(f"longest('{word}')")
+        if word in map:
+            return map[word]
+        
+        map[word] = 1
+        wl = len(word)
+        for j in range(wl):
+            nw = word[:j] + word[j+1:]
+            idx = Solution.find(words, nw)
+            if idx != -1:
+                #print(f"{nw} found at index {idx}")
+                map[word] = max(map[word], 1 + Solution.longest(nw, words, map))
+        return map[word]
+
     def longestStrChain(self, words: List[str]) -> int:
-        pass
+        words.sort(key=lambda str: len(str), reverse=True)
+        #print(words)
+        n = len(words)
+        lss = 1
+        i = 0
+        self.map = {}
+        while i < n:
+            if words[i] is None:
+                i += 1
+                continue          
+            lssw = Solution.longest(words[i], words, self.map)
+            #print(f"= {lssw}")
+            #print(f"map = {self.map}")
+            if lssw > lss:
+                lss = lssw
+            i += 1
+        return lss
 
 
 if __name__ == '__main__':
